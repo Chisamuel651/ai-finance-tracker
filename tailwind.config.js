006ada7f2f2df4af0e92286fd3bbf5b775df78a7
Chisamuel651 <chijoe0306@gmail.com>
2024-07-18 10:41:42 +0100

    second commit of project

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#ef233c",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
